c926f56d787a070693f65c02ae0e95260e248c30
Nils Winter <nils.r.winter@uni-muenster.de>
2025-05-21 15:46:50 +0200

    Add inner cv and hp optimization to test suite

--- tests/test_cpm_regression.py
+++ tests/test_cpm_regression.py
@@ -9,18 +9,19 @@ from cpm.simulate_data import simulate_regression_data_scenarios
 from cpm.edge_selection import UnivariateEdgeSelection, PThreshold
 from cpm.cpm_analysis import CPMRegression
 from cpm.utils import check_data
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, ShuffleSplit
 
 
 class TestCPMRegression(unittest.TestCase):
     def setUp(self):
         super(TestCPMRegression, self).setUp()
         univariate_edge_selection = UnivariateEdgeSelection(edge_statistic='pearson',
-                                                            edge_selection=[PThreshold(threshold=[0.05],
+                                                            edge_selection=[PThreshold(threshold=[0.01, 0.05],
                                                                                        correction=[None])])
         # setup an instance of CPMRegression just to initialize the logger instance
         self.cpm = CPMRegression(results_directory='./tmp',
                                  cv=KFold(n_splits=10, shuffle=True, random_state=42),
+                                 inner_cv=ShuffleSplit(n_splits=1, random_state=42),
                                  edge_selection=univariate_edge_selection,
                                  n_permutations=2,
                                  impute_missing_values=True)
