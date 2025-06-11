0ad2e579a1d127a29fcedbd9f75f9b219e62d0a2
Nils Winter <nils.r.winter@uni-muenster.de>
2025-06-11 18:28:35 +0200

    Update example

--- examples/example_simulated_data.py
+++ examples/example_simulated_data.py
@@ -11,15 +11,15 @@ X, y, covariates = simulate_regression_data(n_features=1225, n_informative_featu
                                             noise_level=0.1)
 
 univariate_edge_selection = UnivariateEdgeSelection(edge_statistic='pearson',
-                                                    edge_selection=[PThreshold(threshold=[0.05, 0.01],
+                                                    edge_selection=[PThreshold(threshold=[0.05],
                                                                                correction=[None])],
                                                     t_test_filter=False)
 
 cpm = CPMRegression(results_directory='./tmp/example_simulated_data',
-                    cv=RepeatedKFold(n_splits=10, n_repeats=1, random_state=42),
+                    cv=RepeatedKFold(n_splits=10, n_repeats=5, random_state=42),
                     edge_selection=univariate_edge_selection,
                     inner_cv=ShuffleSplit(n_splits=1, test_size=0.2, random_state=42),
-                    n_permutations=2,
+                    n_permutations=50,
                     atlas_labels='atlas_labels.csv',
                     #atlas_labels=None,
                     select_stable_edges=False)
